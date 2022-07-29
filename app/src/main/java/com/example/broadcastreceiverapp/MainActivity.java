655f3e1d562d96cbb8791f4614d55e81c796e976
Micoder-dev <micoder.com@gmail.com>
2022-07-29 20:37:56 +0530

    initial update

---
+++ app/src/main/java/com/example/broadcastreceiverapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.broadcastreceiverapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
